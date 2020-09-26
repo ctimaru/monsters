9019a4dd6ac1766efad2f5b14240a80f8c4db847
YOUR_USERNAME <c.timaru@gmail.com>
2020-09-26 14:08:49 +0200

    ok

---
+++ build/precache-manifest.b19c08e2016870b734b5f5c4cec013dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93802d4ccd581813cbb9b2732aee0644",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "29c7411d24a38acb3d59",
+    "url": "/monsters/static/css/main.81f2c694.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29c7411d24a38acb3d59",
+    "url": "/monsters/static/js/main.c3d52ce2.chunk.js"
+  },
+  {
+    "revision": "03919b83b878cef951f1",
+    "url": "/monsters/static/js/runtime-main.cc0b5fe4.js"
+  }
+]);
